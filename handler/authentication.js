d37a553215f1ef386d81bd720b86459ddeada88a
Dave9123 <dave.turmawan@outlook.com>
2024-03-23 10:54:37 +0700

    going with Discord authentication wooooo

--- handler/authentication.js
+++ handler/authentication.js
@@ -6,11 +6,16 @@ async function authenticateUser(json) {
     if (json === undefined) {
         throw new Error('No JSON data provided');
     } else {
-        const user = await database.poolQuery('SELECT * FROM authentication WHERE id = ?', [json.id]);
-        const profile = `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.png`;
+        const user = await database.poolQuery('SELECT * FROM authentication WHERE userid = ?', [json.id]);
         if (user.length === 0) {
-            await database.poolQuery('INSERT INTO users (id, username, globalname, avatar) VALUES (?, ?)', [json.id, json.username, json.global_name, json.avatar]);
+            await database.poolQuery('INSERT INTO authentication (userid, username, globalname, avatar) VALUES (?, ?)', [json.id, json.username, json.global_name, `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.png`]);// ?size=1024
+        } else if (user.length > 1) {
+            throw new Error('Multiple users found with the same ID');
+        } else {
+            await database.poolQuery('UPDATE authentication SET username = ?, globalname = ?, avatar = ? WHERE userid = ?', [json.username, json.global_name, `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.png`, json.id]);
         }
+        const token = jwt.sign({ id: json.id }, process.env.JWT_SECRET, { expiresIn: '30d' }); // Expires in 30 days
+        return token;
     }
 };
 
